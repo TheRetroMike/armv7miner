ba59901d932921550ffc6a6726b19442302db10c
RetroMike <michaeldlesk@gmail.com>
2024-10-18 18:29:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ git build-essential cmake libuv1-dev libhwloc-dev -y
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y
 
-git clone https://github.com/glukolog/cpuminer-opt.git
+git clone https://github.com/tpruvot/cpuminer-multi -b linux
 cd cpuminer-opt
 ./autogen.sh
-./configure CFLAGS="-O3 -march=armv7l -mtune=cortex-a53" --with-curl --with-crypto
+#./configure CFLAGS="-O3 -march=armv7l -mtune=cortex-a53" --with-curl --with-crypto
+./configure --with-crypto --with-curl --disable-assembly CC=gcc CXX=g++ CFLAGS="-fPIC -Ofast -fuse-linker-plugin -ftree-loop-if-convert-stores -march=armv7" LDFLAGS="-march=native"
 make
 
 git clone https://github.com/xmrig/xmrig.git
@@ -15,7 +16,7 @@ mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j4
 
-printf '%s\n' '#!/bin/bash' 'cpuminer -a yespower -o stratum+tcp://xxx:1234 -u xxx -p x' | sudo tee -a cpuminer-opt/start.sh
+printf '%s\n' '#!/bin/bash' 'cpuminer -a yespower -o stratum+tcp://xxx:1234 -u xxx -p x' | sudo tee -a cpuminer-multi/start.sh
 chmod +x cpuminer-opt/start.sh
 
 printf '%s\n' '#!/bin/bash' 'build/xmrig -a gr -o stratum+tcp://xxx:1234 -u xxx -p x' | sudo tee -a xmrig/start.sh
